40279b9f8b7f749af056307c313163c620ff23b9
m1cha3l-s <254994195+m1cha3l-s@users.noreply.github.com>
2026-03-24 08:57:11 +0100

    Update occupancy_sensing.ino

--- Firmwares/1. Indicator sensor/occupancy_sensing/occupancy_sensing.ino
+++ Firmwares/1. Indicator sensor/occupancy_sensing/occupancy_sensing.ino
@@ -1,4 +1,12 @@
-#include 
+#include <WiFi.h>
+#include <esp_now.h>
+#include <Adafruit_NeoPixel.h>
+#include <mmwave_for_xiao.h>
+#include <SoftwareSerial.h>
+
+Adafruit_NeoPixel sensor(2, 9, NEO_GRB + NEO_KHZ800);
+
+uint8_t broadcastAddress[] = {0x11, 0x22, 0x33, 0x44, 0x55, 0x66};
 
 void setup() {
   // put your setup code here, to run once:
